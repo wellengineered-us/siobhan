15e600d92e41d6afaa8f1f5d817b472237bf0b6c
Daniel P. Bullington <daniel.bullington@wellengineered.us>
2022-05-16 21:42:57 -0500

    Encoding normalization to UTF8.

--- src/WellEngineered.Siobhan.Deceitful/Strategies/ScriptObfuscationStrategy.cs
+++ src/WellEngineered.Siobhan.Deceitful/Strategies/ScriptObfuscationStrategy.cs
@@ -1,5 +1,5 @@
 ///*
-//	Copyright ©2002-2017 Daniel P. Bullington (dpbullington@gmail.com)
+//	Copyright Â©2002-2017 Daniel P. Bullington (dpbullington@gmail.com)
 //	Distributed under the MIT license: http://www.opensource.org/licenses/mit-license.php
 //*/
 
