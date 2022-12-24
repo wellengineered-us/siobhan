ae8ee142e8e48593184014adefae244d6e7b280e
Daniel P. Bullington <daniel.bullington@wellengineered.us>
2022-12-23 22:45:16 -0600

    Weird git issue.

---
+++ src/WellEngineered.Siobhan.Textual/Lined/ILinedTextualFieldSpec.cs
@@ -0,0 +1,12 @@
+/*
+	Copyright ©2020-2022 WellEngineered.us, all rights reserved.
+	Distributed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+*/
+
+namespace WellEngineered.Siobhan.Textual.Lined
+{
+	public interface ILinedTextualFieldSpec
+		: ITextualFieldSpec
+	{
+	}
+}
